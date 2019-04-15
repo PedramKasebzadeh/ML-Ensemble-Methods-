0daf0e970d360bdb79d1a54d76e6bf5c6fa42563
¨PedramKasebzadeh¨ <pedka102@su15-124.ad.liu.se>
2019-04-15 13:08:30 +0200

    plots

--- Methods.R
+++ Methods.R
@@ -16,7 +16,7 @@ test <- spam[-id,]
 
 # diffrent number of trees 
 ntrees <- seq(10,100,by=10)
-output <- data.frame(number_of_trees = 0, Error = 0)
+output <- data.frame(number_of_trees = 0, rfError = 0)
 row <- 1
 # a loop to get the result for difrent number of trees 
 for(i in ntrees){
@@ -24,17 +24,51 @@ for(i in ntrees){
   yhat <- predict(RF,newdata=test)
   # confusion matrix
   cm <- table(test$Spam, yhat)
-  Error <- (cm[2,1]+cm[1,2])/sum(cm)
-  output[row,] <- c(i,Error)
+  rfError <- (cm[2,1]+cm[1,2])/sum(cm)
+  output[row,] <- c(i,rfError)
   row <- row + 1
 }
 # PLOTing the results 
 ggplot() +
   geom_line(data = output, mapping = aes(x = number_of_trees
-                                         , y = Error)
+                                         , y = rfError)
             , size = 1.5, color = 'forestgreen' ) +
   theme_light() + 
   scale_x_discrete(limits = seq(from = 10, to = 100, by = 10)) +
   ggtitle( 'Misclassification rate by random forest algorithim with
            different number of trees' ) +
   theme(plot.title = element_text(hjust = 0.5))
+
+# Adaboost 
+row <- 1
+output2 <- data.frame(number_of_iterations = 0, Error = 0)
+
+for (i in ntrees){
+  ada <- blackboost(formula = Spam~., data = train,family =Binomial(type = c("adaboost")), control = boost_control(mstop = i))
+                    Y_hat <- predict(object = ada
+                                     , newdata = test
+                                     , type = 'class')
+                    cm <- table(test$Spam, Y_hat)
+                    Error <- (cm[1,2]+cm[2,1])/sum(cm)
+                    output2[row,] <- c(i,Error)
+                    row <- row + 1
+}
+#----- Plot
+
+
+
+
+
+Finaldf <- data.frame(number_of_iterations=seq(10,100,by=10),Error=output2$Error,rfError=output$rfError)
+ggplot(data = Finaldf) +
+  geom_line(mapping = aes(x = number_of_iterations
+                                          , y = Error)
+            , size = 1.5, color = 'deepskyblue1' ) +
+ geom_line(mapping = aes(x = number_of_iterations
+                                         , y = rfError)
+            , size = 1.5, color = 'forestgreen' ) 
+theme_light()+ 
+
+
+
+
